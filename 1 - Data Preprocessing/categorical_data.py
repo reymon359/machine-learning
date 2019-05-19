2684cc171dfd61bcff21d4a5f8646534aaff3169
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-19 13:25:41 +0200

    Scaling data

--- 1 - Data Preprocessing/categorical_data.py
+++ 1 - Data Preprocessing/categorical_data.py
@@ -10,13 +10,13 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Taking care of missing data
+# Taking care of missing data (giving missing data an average value)
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-# Encoding categorical data
+# Encoding categorical data (not numbers)
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
@@ -31,3 +31,10 @@ y = labelencoder_y.fit_transform(y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 # test_size is the size of the data that will go to the test set. The rest will go to the training set. (0.2 = 20%)
+
+# Feature Scaling (values in same scale for no dominant variable)
+# test set no fit because it is already done in the training one
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
