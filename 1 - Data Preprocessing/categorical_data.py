213010ce0ff39d24aa108108ccaa01ccdb124366
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-19 13:32:49 +0200

    Commenting some stuff I did before

--- 1 - Data Preprocessing/categorical_data.py
+++ 1 - Data Preprocessing/categorical_data.py
@@ -1,26 +1,28 @@
 # Data Preprocessing
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+# We separate the dependent and the independent variables
+X = dataset.iloc[:, :-1].values # Independent
+y = dataset.iloc[:, 3].values # Dependent
 
 # Taking care of missing data (giving missing data an average value)
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
-imputer = imputer.fit(X[:, 1:3])
+imputer = imputer.fit(X[:, 1:3]) # Just the columns with the missing values
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data (not numbers)
-# Encoding the Independent Variable
+# Encoding the Independent Variable (countries)
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+# Using Dummy encoding (splitting in to columns with same values 0 and 1)
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 # Encoding the Dependent Variable
