476bf49cf96d08b2404c133565f9c6eefe275c99
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-25 18:25:47 +0200

    preprocessing the data

---
+++ 2 - Regression/2 Multiple linear regression/multiple_linear_regression.py
@@ -0,0 +1,38 @@
+# Multiple Linear Regression
+
+
+# Importing the libraries
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+# We separate the dependent and the independent variables
+X = dataset.iloc[:, :-1].values # Independent
+y = dataset.iloc[:, 4].values # Dependent
+
+# Encoding categorical data (not numbers)
+# Encoding the Independent Variable (countries)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+# Using Dummy encoding (splitting in to columns with same values 0 and 1)
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap 
+X = X[:, 1:] # We use all dummy variables but 1
+# Most of libraries already do it so it is not necessary
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# test_size is the size of the data that will go to the test set. The rest will go to the training set. (0.2 = 20%)
+
+# Feature Scaling (values in same scale for no dominant variable)
+# test set no fit because it is already done in the training one
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
