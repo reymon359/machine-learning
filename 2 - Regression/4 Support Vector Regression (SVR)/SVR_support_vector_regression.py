c95183a0c54e73ba6fae3a2f75eaca3a31086b4c
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-31 11:03:03 +0200

    Using regression template with SVR

---
+++ 2 - Regression/4 Support Vector Regression (SVR)/SVR_support_vector_regression.py
@@ -0,0 +1,43 @@
+# SVR Support Vector Regression
+
+# Importing the libraries
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+# We separate the dependent and the independent variables
+X = dataset.iloc[:, 1:2].values # The 1:2 is to obtain a matrix not a vector
+y = dataset.iloc[:, 2].values # Dependent
+
+# We will not split the dataset for 2 reasons
+# 1 The dataset is too small
+# 2 We need to be as acurate as possible so we let it stay as it is
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+# test_size is the size of the data that will go to the test set. The rest will go to the training set. (0.2 = 20%)
+
+# Feature Scaling (values in same scale for no dominant variable)
+# test set no fit because it is already done in the training one
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+# Fitting SVR to the dataset 
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf') # rbf is the most common for non linear
+regressor.fit(X, y)
+
+# Predicting a new result with Polynomial Regression
+y_pred = regressor.predict([[6.5]])  
+
+# Visualizing the SVR  results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X),  color = 'blue') 
+plt.title('Truth or bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary') 
+plt.show()
