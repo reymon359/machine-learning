bc360132d77d898d86c857eb03214df9f70d8a59
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-10 17:34:25 +0200

    Predicting the Test set results

--- 3 - Classification/1 Logistic Regression/logistic_regression.py
+++ 3 - Classification/1 Logistic Regression/logistic_regression.py
@@ -33,3 +33,7 @@ X_test = sc_X.transform(X_test)
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+# y_pred = vector of predictions of each of the test set observations.
+y_pred = classifier.predict(X_test)
