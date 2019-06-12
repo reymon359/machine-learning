f83e3039d2be9853bc1456154597d708b5fb8889
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-12 07:50:35 +0200

    pasting the classification template code

--- 3 - Classification/2 K Nearest Neighbors/knn.py
+++ 3 - Classification/2 K Nearest Neighbors/knn.py
@@ -1,4 +1,81 @@
 # K-Nearest Neighbours (K-NN)
 
 
+# Importing the libraries
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+# We separate the dependent and the independent variables
+X = dataset.iloc[:, [2, 3]].values # Independent
+y = dataset.iloc[:, 4].values # Dependent
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+# test_size is the size of the data that will go to the test set. The rest will go to the training set. (0.25 = 25%)
+
+# Feature Scaling (values in same scale for no dominant variable)
+# test set no fit because it is already done in the training one
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Fitting the classifier to the Training Set
+# CREATE CLASSIFIER HERE
+
+# Predicting the Test set results
+# y_pred = vector of predictions of each of the test set observations.
+y_pred = classifier.predict(X_test)
+
+# Making the confusion matrix
+# Now we will evaluate the predictions
+from sklearn.metrics import confusion_matrix 
+cm = confusion_matrix(y_test, y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train # New variables containing the sets
+# We now select the step 0.01 to increase the resolution
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+# Creating a contour to see the limit of the regions
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+# Now we plot the limits
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+# Here we plot all the data points
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+# The titles
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
 
