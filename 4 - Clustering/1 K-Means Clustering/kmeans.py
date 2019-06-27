08cdc579e8bc2d2af6fafb4e2e02f9de20dcbac9
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-27 08:53:43 +0200

    importing libraries and data

--- 4 - Clustering/1 K-Means Clustering/kmeans.py
+++ 4 - Clustering/1 K-Means Clustering/kmeans.py
@@ -1,2 +1,11 @@
 # K-Means Clustering
 
+# Importing the libraries
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+# We separate the dependent and the independent variables
+X = dataset.iloc[:, [3, 4]].values # Independent
