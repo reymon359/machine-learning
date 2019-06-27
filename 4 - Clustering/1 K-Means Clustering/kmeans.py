526b997498bbd8bf485fe776fe1f1994151bf711
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-27 19:37:19 +0200

    fitting kmeans

--- 4 - Clustering/1 K-Means Clustering/kmeans.py
+++ 4 - Clustering/1 K-Means Clustering/kmeans.py
@@ -9,3 +9,15 @@ import pandas as pd # To import and manage datasets
 dataset = pd.read_csv('Mall_Customers.csv')
 # We separate the dependent and the independent variables
 X = dataset.iloc[:, [3, 4]].values # Independent
+
+# Using the elbow method to find the optimal number of clusters
+from sklean.cluster import kMeans
+# We will make a loop to go throught the 10 iterations
+wcss = []
+for i in range(1, 11):
+    # First we will fit the Kmeans algorithm to out data X
+    kmeans = KMeans(n_clusters = i, init ='k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(X)
+    # Second we will compute the wcss (within-cluster sums of squares)  and append to our wcss[] 
+    wcss.append(kmeans.inertia_)
+    
