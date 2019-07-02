5f136dbd197014ebe73afd2ecdd0dfd0d45c5dce
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-02 07:44:23 +0200

    Using and plotting the Dendrogram

--- 4 - Clustering/2 Hierarchical Clustering/hierarchical_clustering.py
+++ 4 - Clustering/2 Hierarchical Clustering/hierarchical_clustering.py
@@ -8,3 +8,12 @@ import pandas as pd # To import and manage datasets
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values 
+
+# Using the Dendrogram to fing the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+# We now select the data and the method for the dendrogram
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
