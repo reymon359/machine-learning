81c08932a137abbbc26115085e292843852159d3
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-02 07:35:54 +0200

    importing libraries

--- 4 - Clustering/2 Hierarchical Clustering/hierarchical_clustering.py
+++ 4 - Clustering/2 Hierarchical Clustering/hierarchical_clustering.py
@@ -1,2 +1,6 @@
 # Hierarchical Clustering
 
+# Importing the libraries
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
