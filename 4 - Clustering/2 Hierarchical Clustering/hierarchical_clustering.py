ea10e64af7daad2745a5be3af12437749e17dd36
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-02 07:38:07 +0200

    importing dataset

--- 4 - Clustering/2 Hierarchical Clustering/hierarchical_clustering.py
+++ 4 - Clustering/2 Hierarchical Clustering/hierarchical_clustering.py
@@ -4,3 +4,7 @@
 import numpy as np #  To work with mathematical numbers.
 import matplotlib.pyplot as plt #  To work with plots
 import pandas as pd # To import and manage datasets
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values 
