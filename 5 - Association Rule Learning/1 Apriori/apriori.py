435f4906add68b7f18dfdd41b353015d529ac1d5
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-03 08:22:58 +0200

    Training Apriori on the dataset

--- 5 - Association Rule Learning/1 Apriori/apriori.py
+++ 5 - Association Rule Learning/1 Apriori/apriori.py
@@ -15,3 +15,9 @@ transactions = []
 for i in range(0, 7501): # Going through all the transactions
     transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
     
+# Taining Apriori on the dataset
+from apyori import apriori
+# product bought 3 times a day = 3x7/7500 = 0.0028 = min_support
+# min confidence of 0.2 goes well with the 0.003 min support
+# For the min lift we are looking for rules with at least a 3
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
