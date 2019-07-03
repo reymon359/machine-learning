3fc5f9ed1a80b8bd768b9bdc4c04288a3d62284e
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-03 08:09:50 +0200

    transactions list of lists

--- 5 - Association Rule Learning/1 Apriori/apriori.py
+++ 5 - Association Rule Learning/1 Apriori/apriori.py
@@ -9,3 +9,9 @@ import pandas as pd # To import and manage datasets
 # Here python thought wrongly and set the first transaction names as headers
 # So we put the header = None to tell him there are no headers in the dataset
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+
+# Now we will create a list of lists because is what we need, not a dataframe
+transactions = []
+for i in range(0, 7501): # Going through all the transactions
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+    
