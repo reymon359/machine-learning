f0ae1b51d81b4922168f918839d961dd1ba439d7
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-03 08:34:53 +0200

    Visualizing results

--- 5 - Association Rule Learning/1 Apriori/apriori.py
+++ 5 - Association Rule Learning/1 Apriori/apriori.py
@@ -15,9 +15,19 @@ transactions = []
 for i in range(0, 7501): # Going through all the transactions
     transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
     
-# Taining Apriori on the dataset
+# Training Apriori on the dataset
 from apyori import apriori
 # product bought 3 times a day = 3x7/7500 = 0.0028 = min_support
 # min confidence of 0.2 goes well with the 0.003 min support
 # For the min lift we are looking for rules with at least a 3
+# These ones before depend on the dataset 
 rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualizing the results
+results = list(rules) # To see a list of the rules found by the algorithm
+# The rules are sorted by relevance obtained by a combination of support, confidence and lift
+
+# Turning them in to a list of string to visualize them better
+results_list = []
+for i in range(0, len(results)):
+    results_list.append('RULE:\t' + str(results[i][0]) + '\nSUPPORT:\t' + str(results[i][1]) + '\n' + str(results[i][2]))
