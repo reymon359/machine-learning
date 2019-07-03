bba9332e97d53a1315980ef054ea24bfd0d5986a
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-03 08:01:02 +0200

    Importing libraries and dataset

--- 5 - Association Rule Learning/1 Apriori/apriori.py
+++ 5 - Association Rule Learning/1 Apriori/apriori.py
@@ -1,2 +1,11 @@
-# -*- coding: utf-8 -*-
+# Apriori
 
+# Importing the libraries
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
+
+# Importing the dataset
+# Here python thought wrongly and set the first transaction names as headers
+# So we put the header = None to tell him there are no headers in the dataset
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
