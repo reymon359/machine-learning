5cfc00924283282db78129aa71f7461407a2d9cc
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-05 20:12:04 +0200

    importing libraries and dataset

---
+++ 6 - Reinforcement Learning/1 Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -0,0 +1,11 @@
+# Upper Confidence Bound
+
+# Importing the libraries
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
+
+# Importing the dataset
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+
