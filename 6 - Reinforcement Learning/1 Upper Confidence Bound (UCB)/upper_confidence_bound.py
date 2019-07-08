4668f740f3cd88d1f7a3a33adb7448b501448187
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-08 07:29:34 +0200

    step 1 UCB

--- 6 - Reinforcement Learning/1 Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ 6 - Reinforcement Learning/1 Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -8,4 +8,10 @@ import pandas as pd # To import and manage datasets
 # Importing the dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
+# Implementing UCB (no external packages)
+# Number of times ad i was selected up to round n
+numbers_of_selections = [0] * d
+# Sum of rewards of the ad i up to round n
+sums_of_rewards = [0] * d
+
 
