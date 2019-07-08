0ed0c41d7abbe2de4d3ac7e9fe589c2e1e2b62e7
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-08 07:48:54 +0200

    step 2 UCB

--- 6 - Reinforcement Learning/1 Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ 6 - Reinforcement Learning/1 Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -9,9 +9,31 @@ import pandas as pd # To import and manage datasets
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 # Implementing UCB (no external packages)
+import math
+N = 10000
+d = 10 # Number of ads
+ads_selected = [] # Ads selected on each iteration
+
+# Step 1
 # Number of times ad i was selected up to round n
 numbers_of_selections = [0] * d
 # Sum of rewards of the ad i up to round n
 sums_of_rewards = [0] * d
 
-
+# Step 2
+# We need to compute for each version of the ad the 
+# average reward and the confidence interval
+for n in range(0, N):
+    ad = 0 # Ad with the max_upper_bound 
+    max_upper_bound = 0
+    for i in range(0, d):
+        # If the ad version i was selected at least once
+        if (numbers_of_selections[i] > 0):
+            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
+            delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i 
