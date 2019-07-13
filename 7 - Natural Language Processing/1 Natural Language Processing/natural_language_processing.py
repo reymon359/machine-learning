c451cd78396d20b5614787ae84ac0a6491fb2120
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-13 12:00:33 +0200

    Importing dataset

--- 7 - Natural Language Processing/1 Natural Language Processing/natural_language_processing.py
+++ 7 - Natural Language Processing/1 Natural Language Processing/natural_language_processing.py
@@ -4,4 +4,10 @@
 import numpy as np #  To work with mathematical numbers.
 import matplotlib.pyplot as plt #  To work with plots
 import pandas as pd # To import and manage datasets
-
+ 
+# Importing the dataset
+# We will use a .tsv file over a .csv because the texts already have commas 
+# and we can not separate by them in csv because we will obtain a wrong dataset
+# delimiter = '\t' to separate by tabs 
+# quoting = 3 to ignore double quote
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
