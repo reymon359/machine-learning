16ff75c8e4f37e5e7553525b4810fc51ac4d756c
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-13 11:50:51 +0200

    Importing Libraries

---
+++ 7 - Natural Language Processing/1 Natural Language Processing/natural_language_processing.py
@@ -0,0 +1,7 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
+
