a7d9802586c821508faef43890ad96ce2b9058f8
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-13 12:29:18 +0200

    removing irrelevant words

--- 7 - Natural Language Processing/1 Natural Language Processing/natural_language_processing.py
+++ 7 - Natural Language Processing/1 Natural Language Processing/natural_language_processing.py
@@ -15,7 +15,19 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 # -- Cleaning the text --
 # We will use a library for it
 import re
+import nltk # Library for the irrelevant words
+nltk.download('stopwords') # list of irrelevant words
+from nltk.corpus import stopwords
 # First param is what we don't want to remove
 # Second param is what to put in the removed character. We will put a space
 # Third param is what we want to clean
 review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0]) 
+# Now we will put all the letters into lowecase
+review = review.lower()
+# Now we will remove irrelevant words like prepositions
+# First we will split all the text into a list of words
+review = review.split()
+# Now that the review is a list of words we will go through it and remove the
+# irrelevant ones according to the nltk stopwords. We have to specify that it
+# is in english. We will also put the list in a set to faster the process.
+review = [word for word in review if not word in set(stopwords.words('English'))]
