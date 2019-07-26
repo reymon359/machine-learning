879f48ab5ee1dafc86f81f9819180a0c4ce4d366
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-26 14:02:46 +0200

    splitting and feature scaling

--- 8 - Deep Learning/1 Artificial Neural Networks (ANN)/ann.py
+++ 8 - Deep Learning/1 Artificial Neural Networks (ANN)/ann.py
@@ -26,5 +26,16 @@ onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# test_size is the size of the data that will go to the test set. The rest will go to the training set. (0.2 = 20%)
+
+# Feature Scaling (values in same scale for no dominant variable)
+# test set no fit because it is already done in the training one
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
 
 
