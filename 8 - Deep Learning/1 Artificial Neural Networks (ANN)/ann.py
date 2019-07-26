f8e8237cf3ac28c57fdf2be05d81552e81425a03
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-26 13:57:58 +0200

    Importing the dataset

--- 8 - Deep Learning/1 Artificial Neural Networks (ANN)/ann.py
+++ 8 - Deep Learning/1 Artificial Neural Networks (ANN)/ann.py
@@ -3,4 +3,16 @@
 # Installing Keras, Tensorflow and Theano
 # conda install -c conda-forge keras
 
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np #  To work with mathematical numbers.
+import matplotlib.pyplot as plt #  To work with plots
+import pandas as pd # To import and manage datasets
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+# We separate the dependent and the independent variables
+X = dataset.iloc[:, 3:13].values # Independent
+y = dataset.iloc[:, 13].values # Dependent
 
