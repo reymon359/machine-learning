a31e23e7c4da3f455519ea2e491c518b7724f2ca
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-28 16:59:17 +0200

    Initialising the CNN

--- 8 - Deep Learning/2 Convolutional Neural Networks (CNN)/cnn.py
+++ 8 - Deep Learning/2 Convolutional Neural Networks (CNN)/cnn.py
@@ -7,8 +7,11 @@
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+from keras.models import Sequential # To inicialize the Neural Network as a sequence of layers
+from keras.layers import Convolution2D # To use in the first step; the Convolution Step. 2D because of images
+from keras.layers import MaxPooling2D # Step 2: Pooling
+from keras.layers import Flatten # Step 3: Flattening
+from keras.layers import Dense # Add the fully connected layers and a classic ANN
+
+# Initialising the CNN
+classifier = Sequential()
