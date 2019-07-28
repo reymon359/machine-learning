04d5251780aed8d638f6a3cdd510c43f64d12625
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-28 16:52:18 +0200

    Importing the Keras libraries and packages

--- 8 - Deep Learning/2 Convolutional Neural Networks (CNN)/cnn.py
+++ 8 - Deep Learning/2 Convolutional Neural Networks (CNN)/cnn.py
@@ -1,2 +1,14 @@
-# -*- coding: utf-8 -*-
+# Artificial Neural Network
 
+# Installing Keras, Tensorflow and Theano
+# conda install -c conda-forge keras
+
+# Given that theere are only images will not have a data prepocessing part
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
